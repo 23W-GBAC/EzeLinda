d1e5fa824d428a3bbfb1b368843d48ddb84702ee
Ezenwa Linda Funa <112171077+EzenwaLinda@users.noreply.github.com>
2024-02-07 08:33:02 +0100

    Create version.sh

---
+++ version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+
+# Add changes to the staging area
+git add .
+
+# Commit changes with the specified message
+git commit -am "recent commit"
+
+# Push changes to the remote repository 
+git push origin main
